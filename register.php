f5d2d65a4f497bbb159e62dd6577973cfc7ded0d
JEVizcaya <jevizcaya@hotmail.com>
2025-02-04 20:16:52 +0100

    creando form reg

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <header>
+        <img src="logo/logo1.webp" alt="">
+        <h1>APPTASK</h1>
+    </header>
+    <form action="" method="post">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" required placeholder="Nombre">
+        <label for="Apellidos"></label>
+    </form>
+</body>
+
+</html>
