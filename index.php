d62378a6e0ddbadcf0034a45b2cfc9d419e273db
PierinaG15 <pierinaagh7@gmail.comecho>
2025-02-04 19:54:36 +0100

    pagina principal

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Principal</title>
 </head>
 <body>
-    
+    <header>
+        <img src="logo/logo1.webp" alt="">
+        <h1>APPTASK</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="login.php">Iniciar Sesión</a></li>
+            <li><a href="registro.php">Registrarse</a></li>
+        </ul>
 </body>
 </html>
