d49cd9475fa7c4923595f14070631d01456b2861
JEVizcaya <jevizcaya@hotmail.com>
2025-02-05 16:13:20 +0100

    bbdd

--- apptask.sql
+++ apptask.sql
@@ -1,57 +1,66 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: apptask
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema apptask
--- -----------------------------------------------------
+--
+-- Table structure for table `tareas`
+--
 
--- -----------------------------------------------------
--- Schema apptask
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `apptask` DEFAULT CHARACTER SET utf8 ;
-USE `apptask` ;
-
--- -----------------------------------------------------
--- Table `apptask`.`usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `apptask`.`usuarios` (
-  `Usuarios_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NULL DEFAULT NULL,
-  `password` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`Usuarios_id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `apptask`.`tareas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `apptask`.`tareas` (
-  `tareas_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255) NULL DEFAULT NULL,
-  `descripcion` VARCHAR(255) NULL DEFAULT NULL,
-  `fecha_creacion` DATE NULL DEFAULT NULL,
-  `estado` VARCHAR(45) NULL DEFAULT NULL,
-  `tareascol` TINYINT(4) NULL DEFAULT NULL,
-  `Usuarios_id` INT(11) NOT NULL,
+DROP TABLE IF EXISTS `tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tareas` (
+  `tareas_id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `fecha_creacion` date DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  `tareascol` tinyint(4) DEFAULT NULL,
+  `Usuarios_id` int(11) NOT NULL,
   PRIMARY KEY (`tareas_id`),
-  INDEX `fk_tareas_Usuarios_idx` (`Usuarios_id` ASC) ,
-  CONSTRAINT `fk_tareas_Usuarios`
-    FOREIGN KEY (`Usuarios_id`)
-    REFERENCES `apptask`.`usuarios` (`Usuarios_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_tareas_Usuarios_idx` (`Usuarios_id`),
+  CONSTRAINT `fk_tareas_Usuarios` FOREIGN KEY (`Usuarios_id`) REFERENCES `usuarios` (`Usuarios_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `usuarios`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `Usuarios_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Usuarios_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-05 15:47:13
