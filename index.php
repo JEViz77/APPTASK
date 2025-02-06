52a76691e0626d094bdd571b5c49c31ee9fcd74f
PierinaG15 <pierinaagh7@gmail.comecho>
2025-02-06 15:37:30 +0100

    arreglo de home

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
     </header>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
         </ul>
