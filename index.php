441e87065a69969b326fa8021be6e86923064607
PierinaG15 <pierinaagh7@gmail.comecho>
2025-02-06 17:01:49 +0100

    ultimos detalles de los diseños de la pagina

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Principal</title>
     <link rel="stylesheet" href="css/nuevo.css">
+    <style>
+        .center-text {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -16,8 +22,20 @@
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
         </ul>
+    </nav>
+    <div id="presentacion">
+        <h1>Gestiona tus tareas como un profesional</h1>
+        <p class="center-text">Esta plataforma ha sido diseñada con el propósito de ofrecer a los usuarios una herramienta integral y fácil de usar para la gestión eficiente de sus tareas y proyectos. Al registrarse en nuestra plataforma, los usuarios podrán acceder a un sistema intuitivo que les permitirá organizar sus actividades y responsabilidades de manera clara y estructurada a lo largo del mes.
+
+Nuestro objetivo principal es facilitar una gestión más efectiva del tiempo, lo cual es esencial tanto para individuos como para equipos. A través de nuestras funcionalidades, los usuarios podrán crear, programar y dar seguimiento a sus tareas y proyectos, asignar prioridades, establecer plazos y realizar un seguimiento del progreso de manera continua. Todo esto con el fin de optimizar la productividad y reducir el estrés asociado a la gestión de múltiples responsabilidades.
+
+En resumen, nuestra plataforma no solo busca ayudar a los usuarios a cumplir con sus objetivos, sino también fomentar una organización más ordenada y efectiva, lo que se traduce en un mayor rendimiento, un mejor control sobre las tareas y proyectos, y, por ende, una experiencia más fluida y satisfactoria en el día a día.
+
+</p>
+    </div>
+    <footer>
+        <p>copyright_2025</p>
+    </footer>
 </body>
-<footer>
-    <p>copyright_2025</p>
-</footer>
 </html>
+
--- main.php
+++ main.php
@@ -3,7 +3,7 @@
         include("partials/cabecera.php");
         ?>
         <section id="tareas">
-          <h3>Página principal de APP Task</h3>
+          <h3>Página principal de APPTASK</h3>
         </section>
         <?php
         include("partials/footer.php");
